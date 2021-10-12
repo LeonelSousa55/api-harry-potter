57c63926d2a829c190637e84c84c58c16e2527f4
Leonel <leonel55sousa@gmail.com>
2021-10-12 14:14:30 -0300

    feat: add delete character endpoint

--- src/main/java/com/dextra/api/harry/potter/services/CharacterService.java
+++ src/main/java/com/dextra/api/harry/potter/services/CharacterService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dextra.api.harry.potter.dto.CharacterDTO;
 import com.dextra.api.harry.potter.entities.Character;
 import com.dextra.api.harry.potter.repositories.CharacterRepository;
+import com.dextra.api.harry.potter.services.exceptions.DataBaseException;
 import com.dextra.api.harry.potter.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -52,11 +55,25 @@ public class CharacterService {
 
 			Character entity = repository.getOne(id);
 			this.copyDtoToEntity(dto, entity);
-			entity = this.repository.save(entity);
+			entity = repository.save(entity);
 			return new CharacterDTO(entity);
 		} catch (EntityNotFoundException e) {
 
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+
+		try {
+
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+
+			throw new DataBaseException("Integrity violation");
 		}
 	}
 
