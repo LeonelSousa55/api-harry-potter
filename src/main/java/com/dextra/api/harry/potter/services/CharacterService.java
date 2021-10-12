3e7632319ff777474460ce027658ab0613818eb6
Leonel <leonel55sousa@gmail.com>
2021-10-12 12:54:37 -0300

    feat: add find by id character endpoint

--- src/main/java/com/dextra/api/harry/potter/services/CharacterService.java
+++ src/main/java/com/dextra/api/harry/potter/services/CharacterService.java
@@ -1,14 +1,18 @@
 package com.dextra.api.harry.potter.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 import com.dextra.api.harry.potter.dto.CharacterDTO;
 import com.dextra.api.harry.potter.entities.Character;
 import com.dextra.api.harry.potter.repositories.CharacterRepository;
+import com.dextra.api.harry.potter.services.exceptions.ResourceNotFoundException;
 
 
 
@@ -23,5 +27,13 @@ public class CharacterService {
 
 		Page<Character> list = repository.findAll(pageRequest);
 		return list.map(x -> new CharacterDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public CharacterDTO findById(Long id) {
+
+		Optional<Character> obj = repository.findById(id);
+		Character entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new CharacterDTO(entity);
 	}
 }
